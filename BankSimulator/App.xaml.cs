addbe927df56648d271d6b6bf84ef5eb5705e4df
Ivan MSI <arigatory@gmail.com>
2021-08-13 03:02:01 +0300

    Add project files.

---
+++ BankSimulator/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using BankSimulator.Core.Regions;
+using BankSimulator.Views;
+using Prism.Ioc;
+using Prism.Regions;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace BankSimulator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        protected override void ConfigureRegionAdapterMappings(RegionAdapterMappings regionAdapterMappings)
+        {
+            base.ConfigureRegionAdapterMappings(regionAdapterMappings);
+            regionAdapterMappings.RegisterMapping(typeof(StackPanel), Container.Resolve<StackPanelRegionAdapter>());
+        }
+    }
+}
