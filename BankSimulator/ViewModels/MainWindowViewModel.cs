94b9454e686a0270970883c49882ad1d00bf5511
Ivan MSI <arigatory@gmail.com>
2021-08-13 06:13:45 +0300

    add some views

--- BankSimulator/ViewModels/MainWindowViewModel.cs
+++ BankSimulator/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,7 @@
-﻿using Prism.Mvvm;
+﻿using ModuleClients.ViewModels;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
 
 namespace BankSimulator.ViewModels
 {
@@ -11,9 +14,42 @@ namespace BankSimulator.ViewModels
             set { SetProperty(ref _title, value); }
         }
 
+        public DelegateCommand ClientsViewCommand { get; set; }
+        public DelegateCommand DiscoveryViewCommand { get; set; }
+
+
+        public ClientsViewModel ClientsVM { get; set; }
+        //public DiscoveryViewModel DiscoveryVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                SetProperty(ref _currentView, value);
+            }
+        }
+
         public MainWindowViewModel()
         {
+            ClientsVM = new ClientsViewModel();
+            //DiscoveryVM = new DiscoveryViewModel();
+            CurrentView = ClientsVM;
 
+            ClientsViewCommand = new DelegateCommand(OnClientsView);
+       
+            //DiscoveryViewCommand = new DelegateCommand<object>(o =>
+            //{
+            //    CurrentView = DiscoveryVM;
+            //});
+
+        }
+
+        private void OnClientsView()
+        {
+            CurrentView = ClientsVM;
         }
     }
 }
--- ModuleClients/ModuleClientsModule.cs
+++ ModuleClients/ModuleClientsModule.cs
@@ -1,5 +1,7 @@
-﻿using Prism.Ioc;
+﻿using ModuleClients.Views;
+using Prism.Ioc;
 using Prism.Modularity;
+using Prism.Regions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,8 +12,16 @@ namespace ModuleClients
 {
     public class ModuleClientsModule : IModule
     {
+        private readonly IRegionManager _regionManager;
+
+        public ModuleClientsModule(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
         public void OnInitialized(IContainerProvider containerProvider)
         {
+            _regionManager.RegisterViewWithRegion("ContentRegion", typeof(DashboardView));
         }
 
         public void RegisterTypes(IContainerRegistry containerRegistry)
