7231f11359cb221670f74a7f100dabcabac8b696
Ivan MSI <arigatory@gmail.com>
2021-08-16 06:13:20 +0300

    add OpenProduct

--- ModuleClients/ViewModels/ClientDetailViewModel.cs
+++ ModuleClients/ViewModels/ClientDetailViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using BankSimulator.Model;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Regions;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,10 +22,56 @@ namespace ModuleClients.ViewModels
                 SetProperty(ref _selectedClient, value);
             }
         }
-        
+
+        private Product _selectedProduct;
+        public Product SelectedProduct
+        {
+            get { return _selectedProduct; }
+            set
+            {
+                SetProperty(ref _selectedProduct, value);
+                CloseProductCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public ObservableCollection<Product> Products { get; set; }
+
+        public DelegateCommand<string> NavigateCommand { get; set; }
+        public DelegateCommand CloseProductCommand { get; set; }
+        public DelegateCommand OpenProductCommand { get; set; }
+
+
+
         public ClientDetailViewModel()
         {
+            CloseProductCommand = new DelegateCommand(CloseProductExecute, CloseProductCanExecute);
+            OpenProductCommand = new DelegateCommand(OpenProductExecute);
+            Products = new ObservableCollection<Product>();
+            
+        }
 
+        private void OpenProductExecute()
+        {
+            var p = new Product{
+                Name = "Новый банковский продукт",
+                Amount = 0,
+                Number = 123,
+                OpenedDate = DateTime.Now,
+                Percent = 7.0
+            };
+            SelectedClient.Products.Add(p);
+            Products.Add(p);
+        }
+
+        private void CloseProductExecute()
+        {
+            SelectedClient.Products.Remove(SelectedProduct);
+            Products.Remove(SelectedProduct);
+        }
+
+        private bool CloseProductCanExecute()
+        {
+            return SelectedProduct != null;
         }
 
         public void OnNavigatedTo(NavigationContext navigationContext)
@@ -31,6 +79,12 @@ namespace ModuleClients.ViewModels
             if (navigationContext.Parameters.ContainsKey("client"))
             {
                 SelectedClient = navigationContext.Parameters.GetValue<Client>("client");
+                Products.Clear();
+                foreach (var prod in SelectedClient.Products)
+                {
+                    Products.Add(prod);
+                }
+                SelectedProduct = Products.FirstOrDefault();
             }
         }
 
--- ModuleClients/ViewModels/DashboardViewModel.cs
+++ ModuleClients/ViewModels/DashboardViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,19 @@ namespace ModuleClients.ViewModels
 {
     public class DashboardViewModel : BindableBase
     {
+        private IRegionManager _regionManager;
+
+        public DelegateCommand<string> NavigateCommand { get; set; }
+
+        public DashboardViewModel(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+            NavigateCommand = new DelegateCommand<string>(Navigate);
+        }
+
+        private void Navigate(string uri)
+        {
+            _regionManager.RequestNavigate("ContentRegion", uri);
+        }
     }
 }
